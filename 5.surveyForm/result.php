44760bd9452883725fccf2eca2ce74898fb64251
Janatbek <janatbek@gmail.com>
2016-07-11 23:00:12 -0700

    moved files into folder

--- 5.surveyForm/result.php
+++ 5.surveyForm/result.php
@@ -6,13 +6,14 @@
 	
 </head>
 <body>
+	
 	<p> Your name is: <?php echo $_POST['first_name']; ?> </p>
 	<p> Your location is: <?php echo $_POST['location']; ?> </p>
 	<p> Your language is: <?php echo $_POST['language']; ?> </p>
 	<p> Your comment is: <?php echo $_POST['comment']; ?> </p>
 	<?php 
 
-var_dump($_GET);
+var_dump($_POST);
 
  ?>
 </body>
